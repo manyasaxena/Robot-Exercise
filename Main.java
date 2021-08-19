20b4524ebbb363aca869a33a18e4eae582a9cbdc
Manya Saxena <68880201+manyasaxena@users.noreply.github.com>
2021-08-19 02:21:11 +0000

    Make a writeInitials method.

--- Main.java
+++ Main.java
@@ -9,16 +9,25 @@ public class Main implements Directions{
     // the bigger the street, the farther north
     World.setSize(20,20);// 20 streets by 20 avenues.
 
+    World.setDelay(5);
 
     // The line below creates a Robot that we will refer to as rob  
     // Find out what the numbers and direction do!
     // Put rob in a better location for your initials.
     Robot rob = new Robot(15,2,South,99);
+
+
 		
     // Want a second robot?  No prob.  They are cheap :)
     //Robot dude = new Robot(7,5,West,9);
     // examples of commands you can invoke on a Robot
-    rob.move();// move one step in the direction it is facing
+    writeInitials(rob);
+     
+    
+  }
+
+  private static void writeInitials(Robot rob){
+        rob.move();// move one step in the direction it is facing
     
     
     // starting the letter R
@@ -138,7 +147,5 @@ public class Main implements Directions{
 
 
 
-     
-    
   }
 }
